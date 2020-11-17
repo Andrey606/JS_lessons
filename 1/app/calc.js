6249e1664a4dd5693ebe759400cfc9b06cb24085
Andrey606 <akuluev@ukr.net>
2020-11-17 22:43:38 +0200

    sdsd

--- 1/app/calc.js
+++ 1/app/calc.js
@@ -1,7 +1,13 @@
 // app/calc.js
+
+
 function sum (arr) {
     return arr.reduce(function(a, b) {
         return a + b
     }, 0)
 }
-module.exports.sum = sum
+module.exports.sum = sum // сделали переменную summ доступной из других модулей
+
+
+//const x = 5;
+// module.exports.x = x; // сделали константу 5 доступной из других модулей
